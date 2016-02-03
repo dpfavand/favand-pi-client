83b5197371611e83278557a0be4054e911e1290b
Daniel Favand <Daniel Favand>
2016-02-03 01:09:31 -0500

    added update loop

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-var config = require('./config.json');
-var express = require('express');
-var socket = require('socket.io-client')(config.socketServer);
+var config  = require('./config');
+var app     = require('express')();
+var server  = require('socket.io-client')(config.socketServer);
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
+var fs      = require('fs');
+
+// using socket as the client to the main server
+// using io as the local server
+
+http.listen(config.clientport, function(){
+    console.log('Listening for connections');
+})
+
+server.on('connect', function(){
+    serverUpdateLoop();
+    console.log('connected to server');
+})
+
+io.on('connection', function(socket){
+    sendImage(socket)
+})
+
+function sendImage(connection){
+    fs.readFile(config.camerajpeg, function(err, buf){
+        if (err) {
+            server.emit('pi_error', err)
+            console.error(err);
+        } else {
+            connection.emit('image', {image: true, buffer: 'data:image/jpeg;base64,' + buf.toString('base64')});   
+        }    
+    }); 
+    
+}
+
+function serverUpdateLoop() {
+    if(server.connected){
+        sendImage(server);
+        console.log(Date.now(), "Sent image to server");
+        setTimeout(serverUpdateLoop, 5 * 60 * 1000);    
+    }
+    
+}
---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.socketServer = 'http://piserver-favand.rhcloud.com:8000';
+config.listenport = 8090;
+config.camerajpeg = 'C:\\dev\\image.jpg';
+
+module.exports = config;
