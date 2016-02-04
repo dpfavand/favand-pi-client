35a07a10f6ff38a5527863ef432f597ee935be47
Daniel Favand <Daniel Favand>
2016-02-03 20:39:43 -0500

    update file location

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {};
 
 config.socketServer = 'http://piserver-favand.rhcloud.com:8000';
 config.listenport = 8090;
-config.camerajpeg = '/home/pi/image.jpg';
+config.camerajpeg = './image.jpg';
 
 module.exports = config;
