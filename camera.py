11109d67963f0d86905184b7e693e58711c6a436
Daniel Favand <Daniel Favand>
2016-02-27 15:01:32 -0500

    back to jpeg

--- camera.py
+++ camera.py
@@ -7,7 +7,7 @@ stream = io.BytesIO()
 
 with picamera.PiCamera() as camera:
     # print("test")
-    camera.capture(stream, format='png')
+    camera.capture(stream, format='jpeg')
     pass
 
 print(base64.b64encode(stream.getvalue()))
