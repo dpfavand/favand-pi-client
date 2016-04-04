f4a3018a013aa72969ebe7c93f93742a2a6c471a
Daniel Favand <Daniel Favand>
2016-04-04 01:01:58 -0400

    reducing resolution hoping to improve performance

--- camera.py
+++ camera.py
@@ -6,6 +6,7 @@ stream = io.BytesIO()
 
 with picamera.PiCamera() as camera:
     # print("test")
+    camera.resolution = (640, 480)
     camera.capture(stream, format='png')
 
 print(base64.b64encode(stream.getvalue()))
